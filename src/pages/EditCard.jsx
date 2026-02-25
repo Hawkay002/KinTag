9db5258c2c231dea645a14349dd4fbb0192c1ee1
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-02-25 17:27:53 +0530

    Update EditCard.jsx

--- src/pages/EditCard.jsx
+++ src/pages/EditCard.jsx
@@ -111,7 +111,7 @@ export default function EditCard() {
       async (position) => {
         try {
           const { latitude, longitude } = position.coords;
-          const res = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`);
+          const res = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&accept-language=en`);
           const data = await res.json();
           setFormData(prev => ({ 
             ...prev, 
