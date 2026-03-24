46b0180457922ed2e9237d674059c18bf91f2070
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-03-24 12:24:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { motionwind } from "motionwind-react/vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
+    motionwind(),
     react(),
     VitePWA({
       registerType: 'prompt', // Tells it to use the manual Update Toast
